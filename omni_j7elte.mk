c4e329fdfef50a9d18cb5484d7cbba8226ab6236
NotNoelChannel <notnoel12345678@gmail.com>
2023-06-12 09:39:50 +0200

    j7elte: Rebase device tree

--- omni_j7elte.mk
+++ omni_j7elte.mk
@@ -1,23 +1,13 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/samsung/j7elte/overlay
-
-LOCAL_PATH := device/samsung/j7elte
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernAl
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/init.recovery.samsungexynos7420.rc:root/init.recovery.samsungexynos7420.rc \
-    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/recovery.fstab
-    
-
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, build/target/product/embedded.mk)
 
-PRODUCT_NAME := omni_j7elte
-PRODUCT_DEVICE := j7elte
-PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := j7elte
+$(call inherit-product, build/target/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/languages_small.mk)
+
+PRODUCT_DEVICE       := j7elte
+PRODUCT_NAME         := omni_j7elte
+PRODUCT_BRAND        := samsung
+PRODUCT_MODEL        := SM-J700
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := Samsung Galaxy J7 (2015)
+TARGET_VENDOR        := samsung
